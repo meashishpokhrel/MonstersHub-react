8899d4b03bdf6c9e915d215977d079566a3a28c6
meashishpokhrel <meashishpokhrel@gmail.com>
2020-05-25 09:30:29 +0545

    Class Componets

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      string: "Ashish Pokhrel"
+    };
+  }
+
+
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello man ashish <code>src/App.js</code> and save to reload.
+          {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={ () => this.setState({ string: "Sita Pokhrel"})}>
+          Change Text
+        </button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
