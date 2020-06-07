b68c483ded6e9be8ce3570b5c0f3c1ff1516ec4a
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-07 12:26:57 +0545

    UNique images for monsters

--- monsters-rolodex/src/componenets/card/card.component.jsx
+++ monsters-rolodex/src/componenets/card/card.component.jsx
@@ -3,7 +3,7 @@ import "./card.style.css";
 
 export const Card = props => (
     <div className="card-container">
-        <img alt="monsters"  src="https://robohash.org/set_set2/?size=180x180" />
+        <img alt="monsters"  src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h1>{props.monster.name}</h1>
         <p>{props.monster.email}</p>
     </div>
